4ed6f5a80af5cb91cca81e9d1f28f725112a0fa9
AntoniRomera <toniromeraluis@gmail.com>
2016-12-05 17:25:23 +0100

    reservas v6

--- reservas.js
+++ reservas.js
@@ -1,9 +1,14 @@
 
 let reservas = (function(inicio) {
     //variables
-    let checkin, checkout, tipo_hab, precio_base = 0, mes = moment().format('MMMM');
+    let checkin, checkout, tipo_hab, precio_base, mes, dia_actual, plazo;
     return {
         //funciones
+        dar_valor: function(){
+            mes = moment().format('MMMM');
+            precio_base = 0;
+            dia_actual = moment.format();
+        },
         precio_base: function(){
             if (tipo_hab == "ind"){
                 precio_base = 30;
@@ -12,15 +17,19 @@ let reservas = (function(inicio) {
                     precio_base = precio_base + precio_añadido;
                 }
             } else {
+                precio_base = 50;
                 if (mes == "June" ||mes == "July" ||mes == "August" ||mes == "September"){
                     reservas.temporada_alta(precio_base);
                     precio_base = precio_base + precio_añadido;
                 }
-                precio_base = 50;
+                
             }
         },
         temporada_alta: function(valor){
             precio_añadido = valor * 0.4;
+        },
+        tiempo_reserva: function() {
+            plazo = moment.duration(end.diff(dia_actual)).asDays();
         }
     }
 })();
