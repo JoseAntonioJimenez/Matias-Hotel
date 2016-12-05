e19e27d98ad4548974cc2e89bc890a069672a188
AntoniRomera <toniromeraluis@gmail.com>
2016-12-05 16:40:17 +0100

    reservas

--- reservas.js
+++ reservas.js
@@ -1,7 +1,19 @@
-var reservas = (function(inicio) {
+
+let reservas = (function(inicio) {
     //variables
-    
+    let checkin, checkout, tipo_hab, precio_base = 0;
     return {
         //funciones
+        precio_base: function(){
+            if (tipo_hab == "ind"){
+                precio_base = 30;
+                
+            } else {
+                precio_base = 50;
+            }
+        },
+        temporada_alta: function(valor){
+            precio_añadido = valor * 0.4;
+        }
     }
 })();
