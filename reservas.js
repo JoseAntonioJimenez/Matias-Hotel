a99ce7d0aac3f257dddbc45a01140c0d107c638f
AntoniRomera <toniromeraluis@gmail.com>
2016-12-05 18:28:10 +0100

    reservas v7

--- reservas.js
+++ reservas.js
@@ -1,35 +1,42 @@
-
-let reservas = (function(inicio) {
+let reservas = (function (inicio) {
     //variables
-    let checkin, checkout, tipo_hab, precio_base, mes, dia_actual, plazo;
+    let checkin, checkout, tipo_hab, precio_base, mes, dia_actual, plazo, descuento, precio_final, dias = new Array(),
+        dias_cont = 0;
     return {
         //funciones
-        dar_valor: function(){
+        dar_valor: function () {
             mes = moment().format('MMMM');
             precio_base = 0;
-            dia_actual = moment.format();
+            dia_actual = moment();
+            checkout = moment(document.getElementById("checkout").value);
+            descuento = 0;
+            plazo = moment.duration(checkout.diff(dia_actual)).asDays();
+            precio_final = 0;
         },
-        precio_base: function(){
-            if (tipo_hab == "ind"){
+        precio_base: function () {
+            if (tipo_hab == "ind") {
                 precio_base = 30;
-                if (mes == "June" ||mes == "July" ||mes == "August" ||mes == "September"){
-                    reservas.temporada_alta(precio_base);
-                    precio_base = precio_base + precio_añadido;
-                }
             } else {
                 precio_base = 50;
-                if (mes == "June" ||mes == "July" ||mes == "August" ||mes == "September"){
-                    reservas.temporada_alta(precio_base);
-                    precio_base = precio_base + precio_añadido;
-                }
-                
             }
+            if (plazo > 7) {
+                reservas.tiempo_reserva(precio_base);
+                precio_final = precio_base - descuento;
+            }
+            if (mes == "June" || mes == "July" || mes == "August" || mes == "September") {
+                reservas.temporada_alta(precio_base);
+                precio_final = precio_final + precio_añadido;
+            }
+
         },
-        temporada_alta: function(valor){
+        temporada_alta: function (valor) {
             precio_añadido = valor * 0.4;
         },
-        tiempo_reserva: function() {
-            plazo = moment.duration(end.diff(dia_actual)).asDays();
+        tiempo_reserva: function (valor) {
+            descuento = valor * 0.1;
+        },
+        calcular_dias: function () {
+
         }
     }
 })();
